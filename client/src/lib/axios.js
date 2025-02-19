bfe4b4861e6c1b32fab940655c2eacdca176a25f
Jay Rathod <202201255@daiict.ac.in>
2025-02-19 16:40:30 +0530

    final co

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "https://woc7-0-adapt.onrender.com/api",
+	baseURL:
+		import.meta.env.MODE === "development"
+			? "http://localhost:5001/api"
+			: "https://woc7-0-adapt.onrender.com/api",
 	withCredentials: true,
 });
